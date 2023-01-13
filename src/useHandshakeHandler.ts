6a5fd25c6ca444231de8a126a1f701820210087d
Liam Martens <hi@liammartens.com>
2023-01-12 19:41:00 -0500

    fix: allow null channel for useHandshakeHandler

--- src/useHandshakeHandler.ts
+++ src/useHandshakeHandler.ts
@@ -2,21 +2,25 @@ import { Types } from 'ably';
 import { useEffect } from 'react';
 import { waitForAttached } from './utils';
 
-export function useHandshakeHandler(channel: Types.RealtimeChannelCallbacks) {
+export function useHandshakeHandler(channel: Types.RealtimeChannelCallbacks | null) {
   useEffect(() => {
-    const handler = (message: Types.Message) => {
-      if (message.name === '__ably-react__handshake__initiate') {
-        channel.publish('__ably-react__handshake__callback', message.data);
-      }
-    };
-    const promise = waitForAttached(channel);
-    promise.then(() => {
-      channel.subscribe(handler);
-    });
+    if (channel) {
+      const handler = (message: Types.Message) => {
+        if (message.name === '__ably-react__handshake__initiate') {
+          channel.publish('__ably-react__handshake__callback', message.data);
+        }
+      };
+      const promise = waitForAttached(channel);
+      promise.then(() => {
+        channel.subscribe(handler);
+      });
 
-    return () => {
-      promise.cancel();
-      channel.unsubscribe(handler);
-    };
+      return () => {
+        promise.cancel();
+        channel.unsubscribe(handler);
+      };
+    }
+
+    return () => {};
   }, []);
 }
