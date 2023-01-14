bf5fe83d4e8bab71cef3a96e7d23537256b89b17
Liam Martens <hi@liammartens.com>
2023-01-14 12:01:26 -0500

    feat: update useAblyClientStatus to use useSyncExternalStore

--- src/useAblyClientStatus.ts
+++ src/useAblyClientStatus.ts
@@ -1,8 +1,5 @@
 import { Realtime } from 'ably';
-import {
-  useCallback, useEffect, useRef, useState,
-} from 'react';
-import { useCallbackRef } from 'use-auto-callback-ref';
+import { useSyncExternalStore } from 'use-sync-external-store';
 
 export enum AblyClientStatus {
   IDLE = 'idle',
@@ -29,71 +26,42 @@ const connectionStateToClientStatus = {
   update: AblyClientStatus.UPDATE,
 };
 
+function subscribeToAllStatusChanges(client: Realtime | null) {
+  return (handler: () => void) => {
+    client?.connection.on('initialized', handler);
+    client?.connection.on('connected', handler);
+    client?.connection.on('connecting', handler);
+    client?.connection.on('disconnected', handler);
+    client?.connection.on('suspended', handler);
+    client?.connection.on('closed', handler);
+    client?.connection.on('closing', handler);
+    client?.connection.on('failed', handler);
+    client?.connection.on('update', handler);
+
+    return () => {
+      client?.connection.off('initialized', handler);
+      client?.connection.off('connected', handler);
+      client?.connection.off('connecting', handler);
+      client?.connection.off('disconnected', handler);
+      client?.connection.off('suspended', handler);
+      client?.connection.off('closed', handler);
+      client?.connection.off('closing', handler);
+      client?.connection.off('failed', handler);
+      client?.connection.off('update', handler);
+    };
+  };
+}
+
 export function useAblyClientStatus(client: Realtime | null) {
-  const [status, setStatus] = useState(AblyClientStatus.IDLE);
-  const resetStatusTimeoutIdRef = useRef<ReturnType<typeof setTimeout> | null>(null);
-
-  const clearResetStatusTimeoutIdRef = useCallbackRef(() => {
-    if (resetStatusTimeoutIdRef.current) {
-      clearTimeout(resetStatusTimeoutIdRef.current);
-      resetStatusTimeoutIdRef.current = null;
-    }
-  }, [resetStatusTimeoutIdRef]);
-
-  // this function only uses refs
-  // no deps needed
-  const resetStatus = useCallback(() => {
-    clearResetStatusTimeoutIdRef.current();
-    resetStatusTimeoutIdRef.current = setTimeout(() => {
-      setStatus(AblyClientStatus.IDLE);
-    }, 1000);
-  }, []);
-
-  const updateStatus = useCallback((incoming: AblyClientStatus) => {
-    clearResetStatusTimeoutIdRef.current();
-    setStatus(incoming);
-  }, []);
-
-  useEffect(() => {
-    resetStatus();
-
-    if (client) {
-      const handleInitialized = () => updateStatus(AblyClientStatus.INITIALIZED);
-      const handleConnected = () => updateStatus(AblyClientStatus.CONNECTED);
-      const handleConnecting = () => updateStatus(AblyClientStatus.CONNECTING);
-      const handleDisconnected = () => updateStatus(AblyClientStatus.DISCONNECTED);
-      const handleSuspended = () => updateStatus(AblyClientStatus.SUSPENDED);
-      const handleClosed = () => updateStatus(AblyClientStatus.CLOSED);
-      const handleClosing = () => updateStatus(AblyClientStatus.CLOSING);
-      const handleFailed = () => updateStatus(AblyClientStatus.FAILED);
-      const handleUpdate = () => updateStatus(AblyClientStatus.UPDATE);
-
-      updateStatus(connectionStateToClientStatus[client.connection.state]);
-      client.connection.on('initialized', handleInitialized);
-      client.connection.on('connected', handleConnected);
-      client.connection.on('connecting', handleConnecting);
-      client.connection.on('disconnected', handleDisconnected);
-      client.connection.on('suspended', handleSuspended);
-      client.connection.on('closed', handleClosed);
-      client.connection.on('closing', handleClosing);
-      client.connection.on('failed', handleFailed);
-      client.connection.on('update', handleUpdate);
-
-      return () => {
-        client.connection.off('initialized', handleInitialized);
-        client.connection.off('connected', handleConnected);
-        client.connection.off('connecting', handleConnecting);
-        client.connection.off('disconnected', handleDisconnected);
-        client.connection.off('suspended', handleSuspended);
-        client.connection.off('closed', handleClosed);
-        client.connection.off('closing', handleClosing);
-        client.connection.off('failed', handleFailed);
-        client.connection.off('update', handleUpdate);
-      };
-    }
-
-    return () => {};
-  }, [client]);
+  const status = useSyncExternalStore(
+    (handler) => subscribeToAllStatusChanges(client)(handler),
+    () => (
+      client?.connection.state
+        ? connectionStateToClientStatus[client.connection.state]
+        : AblyClientStatus.IDLE
+    ),
+    () => AblyClientStatus.IDLE,
+  );
 
   return status;
 }
